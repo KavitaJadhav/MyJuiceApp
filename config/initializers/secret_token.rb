901263652b1eba7936e267ab472a4da9125755e5
Kavita <jadhavkavita206@gmail.com>
2014-07-28 10:43:19 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyJuiceApp::Application.config.secret_key_base = '43e9e20720d6975360a4e3254585b4f07d7c79dfe4a528633bbe1bf6c2b953cbab2a68746bb9fe180bcc916cdc234c2aa3a7093e554ae1582571cf04557ee9a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyJuiceApp::Application.config.session_store :cookie_store, key: '_MyJuiceApp_session'
